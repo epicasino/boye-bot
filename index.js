78630ec235244d781ff62593878162d0f88364a1
unknown <rayjonathanbadua@gmail.com>
2023-01-30 16:15:35 -0800

    use process env instead of config.json, commented out config.json in index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 // eslint-disable-next-line no-unused-vars
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+// const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
